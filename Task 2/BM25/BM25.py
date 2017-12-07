98ab23428a6f2a6e3c7a40feb339fe6595805fea
Sanjay Murali <sanjaymurali@ccs.neu.edu>
2017-12-06 23:36:53 -0500

    Added task2

---
+++ Task 2/BM25/BM25.py
@@ -0,0 +1,308 @@
+import os
+import math
+import operator
+import shutil
+
+import re
+
+CORPUS_DIR = "../Corpus"
+QUERY_FILE = "../queriesRedefined.txt"
+BM25_SCORE_DIR = "BM25 Scores"
+BM25_PSEUDO_SCORE_DIR = "BM25 Psuedo Scores"
+CACM_REL = "cacm.rel.txt"
+
+# for creating inverted index
+FILENAMES_IN_CORPUS = []
+INVERTED_INDEX = dict()
+REL_INFO = dict()
+RELEVANT_DOCS = dict()
+NONRELEVANT_DOCS = dict()
+
+# part of calculating BM25 score
+LENGTH_OF_DOC = dict() # contains the length of each document in the corpus
+AVERAGE_LENGTH_OF_DOC = 0 # average length of a document in the corpus "AVDL"
+
+# read the query file, calculate BM25 score for each query and write them on to disk
+def BM25():
+    queries = open(QUERY_FILE, "r")
+    query = queries.readline()
+    query_id = 1
+    while query != "":
+        score = calculate_BM25(query.split(), query_id)
+        write_score(score, query_id)
+        query_id += 1
+        query = queries.readline()
+
+# The actual BM25 formula.
+def BM25_Formula(n, qf, f, dl, R, r):
+    N = len(LENGTH_OF_DOC) # total number of documents
+    k1 = 1.2
+    k2 = 100
+    b = 0.75
+    K = k1*((1-b) + b*(float(dl)/float(AVERAGE_LENGTH_OF_DOC)))
+    first_part = math.log(((r + 0.5)/(R - r + 0.5))/((n - r + 0.5)/(N - n - R + r + 0.5)))
+    second_part = ((k1 + 1)*f)/(K + f)
+    third_part = ((k2 + 1) * qf)/(k2 + qf)
+    total = first_part*second_part*third_part
+    return total
+
+# given a array of Strings, it calculates the BM25 score for the given query
+def calculate_BM25(query_words, query_id):
+    doc_score = dict()
+    query_term_frequency = dict()
+    # term frequencies for each word in the given query
+    for word in query_words:
+        if not query_term_frequency.has_key(word):
+            query_term_frequency.update({word: 1})
+        else:
+            query_term_frequency[word] += 1
+
+    # building inverted index with only terms in the query
+    query_inverted_index = dict()
+    for term in query_term_frequency:
+        if not INVERTED_INDEX.has_key(term):
+            query_inverted_index.update({term: {}})
+        else:
+            query_inverted_index.update({term: INVERTED_INDEX[term]})
+
+    for term in query_inverted_index:
+        if INVERTED_INDEX.has_key(term):
+            n = len(INVERTED_INDEX[term]) # number of documents containing the term
+            qf = query_term_frequency[term] # query frequency
+            for doc_id in query_inverted_index[term]:
+                f = INVERTED_INDEX[term][doc_id] # frequency of the term in the given document
+                if LENGTH_OF_DOC.has_key(doc_id):
+                    dl = LENGTH_OF_DOC[doc_id] # length of the document, given the docid
+                if REL_INFO.has_key(str(query_id)):
+                    R = len(REL_INFO[str(query_id)])
+                    r = reverse_doc_search(term, query_id)
+                else:
+                    R = 0
+                    r = 0
+                score = BM25_Formula(n, qf, f, dl, R, r) # the actual BM25 score for the given term
+                if doc_id in doc_score:
+                    total_score = doc_score[doc_id] + score # query consists of several words, so total needs to be found
+                    doc_score.update({doc_id: total_score})
+                else:
+                    doc_score.update({doc_id: score})
+
+    doc_score = sorted(doc_score.items(), key=operator.itemgetter(1), reverse=True) # sort them in descending order of score
+    doc_score = doc_score[0:100] # the assignment asks only top 100
+    return doc_score
+
+
+# read the query file, calculate BM25 score for each query and write them on to disk
+def BM25_pseudo():
+    queries = open(QUERY_FILE, "r")
+    query = queries.readline()
+    query_id = 1
+    while query != "":
+        score = calculate_pseudorelevance(query.split(), query_id)
+        write_score(score, query_id)
+        query_id += 1
+        query = queries.readline()
+
+def calculate_pseudorelevance(query_words, query_id):
+    alpha = 1.0
+    beta = 0.75
+    gamma = 0.15
+    query_vector = form_query_vector(query_words)
+    new_query_vector = dict()
+    for term in query_words:
+
+        first_part = alpha * query_vector[term]
+
+        second_part_1 = beta*(1/len(RELEVANT_DOCS[str(query_id)]))
+
+        sum_rel = 0
+        sum_nonrel = 0
+        if INVERTED_INDEX.has_key(term):
+            for doc in RELEVANT_DOCS[str(query_id)]:
+                if doc in INVERTED_INDEX[term]:
+                    sum_rel += INVERTED_INDEX[term][doc]
+            for doc in NONRELEVANT_DOCS[str(query_id)]:
+                if doc in INVERTED_INDEX[term]:
+                    sum_nonrel += INVERTED_INDEX[term][doc]
+
+        second_part = second_part_1 * sum_rel
+
+        third_part_1 = gamma*(1/len(NONRELEVANT_DOCS[str(query_id)]))
+
+        third_part = third_part_1 * sum_nonrel
+        score = first_part + second_part - third_part
+        new_query_vector.update({term: score})
+
+    new_query_list = sorted(new_query_vector.items(), key=operator.itemgetter(1), reverse=True)
+    new_query = ""
+    for term in new_query_list:
+        new_query += term[0] + " "
+    new_query = new_query.rstrip()
+    new_query_words = new_query.split()
+    return calculate_BM25(new_query_words, query_id)
+
+def form_query_vector(query_words):
+    query_vector = dict()
+    for term in query_words:
+        if query_vector.has_key(term):
+            count = query_vector[term] + 1
+            query_vector.update({term: count})
+        else:
+            query_vector[term] = 1
+        if not INVERTED_INDEX.has_key(term):
+            query_vector.update({term: 0})
+    return query_vector
+
+
+# Used in inverted index creation. Create inverted index from corpus
+def process_corpus():
+    global N_GRAM, INVERTED_INDEX
+    retrieve_corpus()
+    inverted_index = dict()
+    for file_name in FILENAMES_IN_CORPUS:
+        file_content = open(CORPUS_DIR + '/' + file_name, "r").read()
+        inverted_index = generate_inverted_index_unigram(file_name, file_content, inverted_index)
+
+    INVERTED_INDEX = inverted_index
+
+
+# Used in inverted index creation. Generate the inverted index for unigram
+def generate_inverted_index_unigram(docid, file_content, inverted_index):
+    # split the document into each word separately
+    global LENGTH_OF_DOC
+    words = file_content.split()
+    LENGTH_OF_DOC[docid] = len(words)
+    for word in words:
+        inverted_index = create_inverted_index(word, docid, inverted_index)
+    return inverted_index
+
+
+# Used in inverted index creation. create the inverted index
+def create_inverted_index(word, docid, inverted_index):
+    # if the word doesnt exists, add it to the inverted index
+    if not inverted_index.has_key(word):
+        inverted_index[word] = {docid: 1}  # initial (docid, tf) format
+    elif inverted_index[word].has_key(docid):
+        # if the word and the docid exists, plus 1 to the term freq. for that docid
+        doc_dict = inverted_index[word]
+        term_freq_value = doc_dict.get(docid)
+        term_freq_value = term_freq_value + 1
+        doc_dict[docid] = term_freq_value
+    else:
+        # if word exists but the docid doesnt, append the current "docid" with initially term freq. of 1 to that word
+        inverted_index[word].update({docid: 1})
+    return inverted_index
+
+
+# Used in inverted index creation. Gets all the file names in the given corpus
+def retrieve_corpus():
+    global FILENAMES_IN_CORPUS
+    if os.path.exists(CORPUS_DIR):
+        file_names = os.listdir(CORPUS_DIR)
+        FILENAMES_IN_CORPUS = file_names
+
+# to get the average document length in the corpus
+def get_average_length_of_doc():
+    global AVERAGE_LENGTH_OF_DOC
+    sum = 0
+    for lengths in LENGTH_OF_DOC.values():
+        sum += lengths
+    avg = float(sum)/float(len(LENGTH_OF_DOC))
+    AVERAGE_LENGTH_OF_DOC = avg
+
+# write scores to files
+def write_score(score, query_id):
+    score_file = open(BM25_PSEUDO_SCORE_DIR+"/Q" + str(query_id) +".txt", "w")
+    builder = ""
+    rank = 1 # from top to bottom
+    for docid, score in score:
+        builder += str(query_id) + "\tQ0\t" + docid + "\t" + str(rank) + "\t" + str(score) + "\tBM25\n"
+        rank += 1
+    builder = builder[0: len(builder)-1] # delete the trailing "\n"
+    score_file.write(builder)
+
+def relevance_info():
+    global REL_INFO
+    rel_file = open(CACM_REL, "r").read()
+    lines = rel_file.split("\n")
+    for line in lines:
+        if len(line) != 0:
+            words = line.split() # word[0] => query_id, word[2] => document_id
+            if REL_INFO.has_key(words[0]):
+                REL_INFO[words[0]].append(words[2])
+            else:
+                REL_INFO[words[0]] = [words[2]]
+
+def reverse_doc_search(term, query_id):
+    documents = INVERTED_INDEX[term]
+    sum = 0
+    for document in REL_INFO[str(query_id)]:
+        document = document + ".txt"
+        for inverted_document in documents:
+            if inverted_document == document:
+                sum = sum + 1
+    return sum
+
+
+def delete_files():
+    if os.path.exists(BM25_SCORE_DIR):
+        shutil.rmtree(BM25_SCORE_DIR)
+    os.mkdir(BM25_SCORE_DIR)
+
+def relevancy():
+    global RELEVANT_DOCS, NONRELEVANT_DOCS
+    filenames = []
+    if os.path.exists(BM25_SCORE_DIR):
+        file_names = os.listdir(BM25_SCORE_DIR)
+        filenames = file_names
+    for filename in filenames:
+        bm25_query_file = open(BM25_SCORE_DIR+"/"+filename, "r")
+        line_counter = 1
+        for line in bm25_query_file.readlines():
+            words_in_line = re.split(r"\t+", line)
+            document = words_in_line[2]
+            query_id = words_in_line[0]
+            if line_counter > 5:
+                if NONRELEVANT_DOCS.has_key(query_id):
+                    NONRELEVANT_DOCS[query_id].append(document)
+                else:
+                    NONRELEVANT_DOCS[query_id] = [document]
+            else:
+                if RELEVANT_DOCS.has_key(query_id):
+                    RELEVANT_DOCS[query_id].append(document)
+                else:
+                    RELEVANT_DOCS[query_id] = [document]
+            line_counter += 1
+
+
+# starter program
+def start():
+    global CORPUS_DIR, QUERY_FILE, CACM_REL
+
+    input_corpus = raw_input("Enter path to the Corpus directory generated from HW3-Task1 (Skip for Default): ")
+    if input_corpus:
+        CORPUS_DIR = input_corpus
+
+    input_query = raw_input("Enter path to the query file (Skip for Default): ")
+    if input_query:
+        QUERY_FILE = input_query
+
+    input_cacm = raw_input("Enter path to the REL file (Skip for Default): ")
+    if input_cacm:
+        CACM_REL = input_cacm
+
+    #delete_files()
+
+    process_corpus() # this function generates the inverted index for unigram for the given corpus
+    get_average_length_of_doc() # this function helps in calculating "AVDL", average document length in the corpus
+    relevance_info()
+    #BM25()
+
+
+    query = "probabilistic algorithms especially those dealing with algebraic and symbolic manipulation  some examples rabiin probabilistic algorithm on finite field siam waztch probabilistic testing of polynomial identities siam  rabinm"
+
+    relevancy()
+    #print calculate_pseudorelevance(query.split(), "35")
+    BM25_pseudo()
+    print "BM25 Scores Generated!"
+
+start()
---
+++ Task 2/gen-cacm-corpus-text.py
@@ -0,0 +1,50 @@
+import re
+from bs4 import BeautifulSoup
+from os import listdir
+
+#using Sanjay's code
+# handling punctuations before a word, such as -1.0 shouldnt be removed while: ".hello" should be removed
+def handle_prefixed_punctuations(word):
+    # dont remove punctuations in a negative and positive, decimal or float number
+    if (re.match(r'^[\-]?[0-9]*\.?[0-9]+$', word)):
+        return word
+    elif (word[:1] == "-") or (word[:1] == ",") or (word[:1] == "."):
+        return word[1:]
+    else:
+        return word
+
+myPath =  "../Project Input/cacm/"
+for file in listdir(myPath):
+    output = open(myPath+file)
+    soup = BeautifulSoup(output, "html.parser")
+    all_text = ''.join(soup.findAll(text=True))
+    all_text = all_text.lower()
+    line = all_text
+    fName =  file.split(".html")[0]
+    temp_array = []
+    # we need to remove trailing and pre-fixed "," , "." and "-" from the words
+    for each_word in line.split():
+        length = len(each_word)
+        # removing trailing punctuations
+        if (each_word[(length - 1): length] == "-") or (each_word[(length - 1): length] == ",") or (
+            each_word[(length - 1): length] == "."):
+            each_word = each_word[:(length - 1)]  # removing the punctuation
+        # removing pre-fixed punctuations
+        each_word = handle_prefixed_punctuations(each_word)
+        thelist = ["_", ":", "/", "!", "?", "#", "^", "*", "~", "&", "(", ")", "[", "]", "{", "}", "'", ";", '"', "$",
+                   "%", "|", ]
+        for punctuation in thelist:
+            each_word = each_word.replace(punctuation, '')
+        temp_array.append(each_word)
+    content_as_array = temp_array
+    line = ' '.join(content_as_array)
+    outputf = open("Corpus/"+fName+".txt","w")
+    outputf.write(line)
+    outputf.close()
+
+
+
+    #soup = BeautifulSoup(fp)
+    #print soup
+    #all_text = ''.join(soup.findAll(text=True))
+    #print all_text
