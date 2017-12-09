a3337a7064e011306ab3a99a626f728ba883e025
akshaysingh1520 <akshaysingh1520@gmail.com>
2017-12-09 12:49:21 -0500

    txt to xls

---
+++ Task 1/Smoothed Query Likelihood Model/Jelinker_txt_to_xls.py
@@ -0,0 +1,35 @@
+import xlwt
+import xlrd
+import os
+
+
+EXCEL_FILE_NAME = "Jelinker_Scores_RESULTS"
+RESULTS_FOLDER_NAME = "Jelinker Scores"
+
+if not os.path.isdir(RESULTS_FOLDER_NAME):
+    print("The Directory with Jelinker Scores doesnt exist!")
+
+
+book = xlwt.Workbook()
+sorted_files = sorted(os.listdir(RESULTS_FOLDER_NAME) , key= lambda x : int(x.split("Q")[1].split(".")[0]) )
+for file in sorted_files:
+    # print(file)
+    name = file.strip().split(".")
+    # print("name", name[0])
+    ws = book.add_sheet(name[0])  # Add a sheet
+    dir_path = os.path.dirname(__file__)
+    rel_path = "/"+RESULTS_FOLDER_NAME+"/" + file
+    filepath = dir_path + rel_path
+    fd = open(filepath)
+    data = fd.readlines()
+    fd.close()
+    for i in range(len(data)):
+        line_data = data[i].split()
+        print(line_data)
+        for k in range(len(line_data)):
+            ws.write(i, k, line_data[k])
+
+if os.path.isfile(EXCEL_FILE_NAME+".xls"):
+    os.remove(EXCEL_FILE_NAME+".xls")
+
+book.save(EXCEL_FILE_NAME+".xls")
